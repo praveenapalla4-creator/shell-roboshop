3599712e240437b14b0eea2ae19ce6747069f94f
praveena <praveenapalla4@gmail.com>
2025-12-15 21:32:38 +0530

    redis

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 USERID=$(id -u)
 R="\e[31m"
@@ -5,8 +6,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
-START_TIME=$(date +%s)
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+START_TIME=$(date +%s)
+
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
@@ -27,22 +29,18 @@ fi
 
 }
 
-dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Disable redis "
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySql server "
 
-dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "Enable redis version 7"
+systemctl enable mysqld
+VALIDATE $? "enable mysql server"
 
-dnf install redis -y  &>>$LOG_FILE
-VALIDATE $? "Installing redis "
+systemctl start mysqld
+VALIDATE $? "Starting MySql server "  
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Allowing remote connections to redis"
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up root password "  
 
-systemctl enable redis 
-
-systemctl start redis 
-VALIDATE $? "Start redis "
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME - $START_TIME))
