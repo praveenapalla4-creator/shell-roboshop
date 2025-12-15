8a4d9e6ba7c8dfb962b8621c91f8294a47e8c409
praveena <praveenapalla4@gmail.com>
2025-12-15 12:44:47 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -18,25 +18,26 @@ do
         RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance : $IP"
-
-# aws route53 change-resource-record-sets \
-#  --hosted-zone-id  $ZONE_ID \
-#  --change-batch '
-#  {
-# 	"Comment":"Updating record set"
-#     ,"Changes": [{
-# 	"Action": "UPSERT",
-# 	"ResourceRecordSet": {
-# 	    "Name": "'$RECORD_NAME'",
-# 	    "Type": "A",
-# 	    "TTL": 1,
-# 	    "ResourceRecords": [{
-# 	        "Value": "'$IP'"
-# 	    }]
-# 	}
-# 	}]
-#     }
-#     '
+    echo "instanceid: $INSTANCE_ID"
+    
+    # aws route53 change-resource-record-sets \
+    #  --hosted-zone-id  $ZONE_ID \
+    #  --change-batch '
+    #  {
+	# "Comment":"Updating record set"
+    # ,"Changes": [{
+	# "Action": "UPSERT",
+	# "ResourceRecordSet": {
+	#     "Name": "'$RECORD_NAME'",
+	#     "Type": "A",
+	#     "TTL": 1,
+	#     "ResourceRecords": [{
+	#         "Value": "'$IP'"
+	#     }]
+	# }
+	# }]
+    # }
+    # '
     done
 	
 
